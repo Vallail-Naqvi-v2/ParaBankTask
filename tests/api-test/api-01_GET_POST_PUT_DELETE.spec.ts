d6be48d650abfe2aac833eaf196163e1e6735c8f
Vallail-dev <vallail.naqvi@v2solutions.com>
2025-04-01 10:59:39 +0530

    updated api to get data from json file and verify the expected response

--- tests/api-test/api-01_GET_POST_PUT_DELETE.spec.ts
+++ tests/api-test/api-01_GET_POST_PUT_DELETE.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, request } from "@playwright/test";
 import { ApiService } from "../../pages/apiTestPage";
+import readJsonFile from "../../utils";
 
 test.describe("API Tests for Posts", () => {
   let apiService: ApiService;
@@ -16,27 +17,32 @@ test.describe("API Tests for Posts", () => {
     const responseData = await response.json();
     expect(Array.isArray(responseData)).toBeTruthy();
     expect(responseData.length).toBeGreaterThan(0);
+    //console.log(responseData);
   });
 
   test("POST Request - Create a new post", async () => {
-    const newPost = { title: "New Post", body: "Post body content", userId: 1 };
-    const response = await apiService.createPost(newPost.title, newPost.body, newPost.userId);
+    const newPost = readJsonFile("./test-data/api-data.json");
+    const response = await apiService.createPost(
+      newPost.postData.title,
+      newPost.postData.body,
+      newPost.postData.userId
+    );
+    //console.log(newPost.postData);
 
     expect(response.status()).toBe(201);
     const responseData = await response.json();
-    expect(responseData.title).toBe(newPost.title);
-    expect(responseData.body).toBe(newPost.body);
-    expect(responseData.userId).toBe(newPost.userId);
+    expect(responseData.title).toBe(newPost.postData.title);
+    expect(responseData.body).toBe(newPost.postData.body);
+    expect(responseData.userId).toBe(newPost.postData.userId);
   });
 
   test("PUT Request - Update a post", async () => {
-    const updatedPost = { title: "Updated Title", body: "Updated Content", userId: 1 };
-    const response = await apiService.updatePost(1, updatedPost);
-
+    const updatedPost = readJsonFile("./test-data/api-data.json");
+    const response = await apiService.updatePost(1, updatedPost.updatedData);
     expect(response.status()).toBe(200);
     const responseData = await response.json();
-    expect(responseData.title).toBe(updatedPost.title);
-    expect(responseData.body).toBe(updatedPost.body);
+    expect(responseData.title).toBe(updatedPost.updatedData.title);
+    expect(responseData.body).toBe(updatedPost.updatedData.body);
   });
 
   test("DELETE Request - Delete a post", async () => {
